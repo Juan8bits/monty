9a7d40b8b7f56886ab5af2b197ce21f67964844c
Juan8bits <juan160820@gmail.com>
2020-12-21 16:00:20 +0000

    Create first files

---
+++ fun2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - Function to do nothing.
+ * @stack: Double pointer with address to double linked list st\
+ack.
+* @n: Number of the new node.
+*/
+void nop(stack_t **stack, unsigned int n)
+{
+	;
+}
---
+++ monty.h
@@ -0,0 +1,54 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Global Variable */
+int n;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void add(stack_t **stack, unsigned int n);
+void sub(stack_t **stack, unsigned int line_n);
+void div_(stack_t **stack, unsigned int line_n);
+void mul(stack_t **stack, unsigned int line_n);
+void mod(stack_t **stack, unsigned int line_n);
+
+void nop(stack_t **stack, unsigned int n);
+void push(stack_t **stack, unsigned int n);
+void pall(stack_t **stack, unsigned int n);
+void pint(stack_t **stack, unsigned int n);
+void pop(stack_t **stack, unsigned int n);
+void swap(stack_t **stack, unsigned int n);
+void free_dlist(stack_t *stack);
+#endif /* MONTY_H */
